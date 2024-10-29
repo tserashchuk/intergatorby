e3abc7821a6f6a73a3d9929e7635af489c1b71c9
tserashchuk <tserashchuk.kirill@gmail.com>
2024-10-29 12:24:41 +0300

    добавил файлы под все страницы и прописал для них урлы

--- bitrixsite/views.py
+++ bitrixsite/views.py
@@ -1,6 +1,95 @@
 from django.shortcuts import render
 from django.views import View
+import requests
 
 class HomeView(View):
     def get(self, request):
         return render(request, 'home.html')
+    
+class Company(View):
+    def get(self, request):
+        return render(request, 'company.html')
+
+class About(View):
+    def get(self, request):
+        return render(request, 'about.html')
+
+class Reviews(View):
+    def get(self, request):
+        return render(request, 'reviews.html')
+
+class Cases(View):
+    def get(self, request):
+        return render(request, 'cases.html')
+
+class Case(View):
+    def get(self, request):
+        return render(request, 'case.html')
+
+class Blog(View):
+    def get(self, request):
+        return render(request, 'blog.html')
+
+class ArticleCategory(View):
+    def get(self, request):
+        return render(request, 'articlecategory.html')
+
+class Article(View):
+    def get(self, request):
+        return render(request, 'article.html')
+
+class Webinars(View):
+    def get(self, request):
+        return render(request, 'webinars.html')
+
+class Webinar(View):
+    def get(self, request):
+        return render(request, 'webinar.html')
+
+class Bitrix(View):
+    def get(self, request):
+        return render(request, 'bitrix.html')
+
+class Services(View):
+    def get(self, request):
+        return render(request, 'services.html')
+
+class Vnedrenie(View):
+    def get(self, request):
+        return render(request, 'vnedrenie.html')
+
+class Dorabotka(View):
+    def get(self, request):
+        return render(request, 'dorabotka.html')
+
+class Podderzka(View):
+    def get(self, request):
+        return render(request, 'podderzka.html')
+
+class Audit(View):
+    def get(self, request):
+        return render(request, 'audit.html')
+
+class Licence(View):
+    def get(self, request):
+        return render(request, 'licence.html')
+
+class Oblako(View):
+    def get(self, request):
+        return render(request, 'oblako.html')
+
+class Korobka(View):
+    def get(self, request):
+        return render(request, 'korobka.html')
+
+class Prodlenie(View):
+    def get(self, request):
+        return render(request, 'prodlenie.html')
+
+class Market(View):
+    def get(self, request):
+        return render(request, 'market.html')
+
+class Contact(View):
+    def get(self, request):
+        return render(request, 'contact.html')
--- intergator/urls.py
+++ intergator/urls.py
@@ -26,4 +26,28 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomeView.as_view(), name='home'),
+    path('company/', views.Company.as_view(), name='company'),
+    path('about/', views.About.as_view(), name='about'),
+    path('reviews/', views.Reviews.as_view(), name='reviews'),
+    path('cases/', views.Cases.as_view(), name='cases'),
+    path('case/', views.Case.as_view(), name='case'),
+    path('blog/', views.Blog.as_view(), name='blog'),
+    path('articlecategory/', views.ArticleCategory.as_view(), name='articlecategory'),
+    path('article/', views.Article.as_view(), name='article'),
+    path('webinars/', views.Webinars.as_view(), name='webinars'),
+    path('webinar/', views.Webinar.as_view(), name='webinar'),
+    path('bitrix/', views.Bitrix.as_view(), name='bitrix'),
+    path('services/', views.Services.as_view(), name='services'),
+    path('vnedrenie/', views.Vnedrenie.as_view(), name='vnedrenie'),
+    path('dorabotka/', views.Dorabotka.as_view(), name='dorabotka'),
+    path('podderzka/', views.Podderzka.as_view(), name='podderzka'),
+    path('audit/', views.Audit.as_view(), name='audit'),
+    path('licence/', views.Licence.as_view(), name='licence'),
+    path('oblako/', views.Oblako.as_view(), name='oblako'),
+    path('korobka/', views.Korobka.as_view(), name='korobka'),
+    path('prodlenie/', views.Prodlenie.as_view(), name='prodlenie'),
+    path('market/', views.Market.as_view(), name='market'),
+    path('contact/', views.Contact.as_view(), name='contact'),
+
+    
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
